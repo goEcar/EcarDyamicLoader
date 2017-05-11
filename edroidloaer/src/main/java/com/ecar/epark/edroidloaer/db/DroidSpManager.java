2bd0e1a76b5a5abd5647c788e1988be14013bac1
goEcar <1234519jin>
2017-05-11 19:03:46 +0800

    1.调整目录结构

--- edroidloaer/src/main/java/com/ecar/epark/edroidloaer/db/DroidSpManager.java
+++ edroidloaer/src/main/java/com/ecar/epark/edroidloaer/db/DroidSpManager.java
@@ -6,15 +6,11 @@ import android.content.SharedPreferences.Editor;
 
 public class DroidSpManager {
 	private final String PREFS_NAME="com.ecar.epark.edroidloaer.db.dbs";
-	private DroidSpManager dbPreferences;
 	private SharedPreferences shareData;
-
-	public final String SpCurrentLocationCityCode = "current_location_city_code";
 
 	public DroidSpManager(Context context){
 		shareData = context.getSharedPreferences(PREFS_NAME, 0);
 	}
-
 
 	public void setJarVersionByName(String jarName,String jarVersion) {
 		Editor editor = shareData.edit();
